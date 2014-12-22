07f597031db2721ff5ee91b4d936f79acc462029
Heshy Roskes <jroskes1@jhu.edu>
2014-12-22 18:01:30 -0500

    First try for LaTeX output

---
+++ implementation/createlatex.py
@@ -0,0 +1,64 @@
+from helperfunctions import removenekudot
+from malbimdatafile import createdict, compileall
+from globalvariables import latexheader
+import codecs
+
+def createlatex(datadict=None, outputfile="latexoutput.tex"):
+    if datadict is None:
+        datadict = createdict(compileall())
+
+    #with codecs.open("latexheader.tex", encoding='utf-8') as f:
+    #    latexstring = f.read()
+    latexstring = latexheader
+
+    strings = ["\\section{Words}", "\\section{Grammar}", "\\section{Concepts}"]
+    for i in range(len(strings)):
+        strings[i] = "\\selectlanguage{english}\n" + strings[i] + "\n\\selectlanguage{hebrew}\n\\begin{description}\n"
+    keys = datadict.keys()
+    keys.sort(key = lambda k: removenekudot(k))
+
+    beginstring = "\\item[%s] \\hfill\n"
+    beginstring += "\\begin{description}\n"
+
+    endstring = "\\end{description}\n\n"
+
+    for key in keys:
+        used = [False, False, False]
+        for reference in datadict[key]:
+            itemstring = ""
+            itemstring += "\\item[" + (reference[0].replace("(", "").replace(")", "")
+                                                   .replace("[", "").replace("]", "")
+                                                   .replace("-", "--")) + "] \\hfill \\\\ \n"
+            itemstring += "\\\\ \n".join(", ".join(b for b in a) for a in reference[1:])
+            itemstring += "\n"
+
+            if reference[0].startswith("("):
+                whichstring = 1
+            elif reference[0].startswith("["):
+                whichstring = 2
+            else:
+                whichstring = 0
+
+            if not used[whichstring]:
+                strings[whichstring] += beginstring % key
+                used[whichstring] = True
+            strings[whichstring] += itemstring
+
+        for i in range(len(strings)):
+            if used[i]:
+                strings[i] += endstring
+
+    for i in range(len(strings)):
+        strings[i] += endstring
+
+    latexstring += strings[0] + strings[1] + strings[2] + "\\end{document}\n"
+
+    latexstring = removenekudot(latexstring)   #Otherwise LaTeX won't compile
+                                               #FIX THIS
+
+    with codecs.open(outputfile, mode='w', encoding='utf-8') as f:
+        f.write(latexstring)
+
+if __name__ == '__main__':
+    createlatex()
+
--- implementation/globalvariables.py
+++ implementation/globalvariables.py
@@ -4,3 +4,22 @@ import compilesynonyms
 
 SYNONYMS = compilesynonyms.Synonyms("../synonyms.txt")
 specialfiles = ["synonyms.txt"]
+
+latexheader = r"""\documentclass[11pt]{article}
+\usepackage{geometry}
+\geometry{verbose,tmargin=1.00in,bmargin=1.00in,lmargin=1.0in,rmargin=1.0in}
+
+\usepackage[utf8x]{inputenc}
+\usepackage[hebrew,english]{babel}
+\usepackage{hebcal}
+
+
+\begin{document}
+\selectlanguage{english}
+\title{Malbim index}
+\author{Heshy Roskes}
+\date{\selectlanguage{english}\today\hfill\selectlanguage{hebrew}\hebrewtoday}
+
+\maketitle
+
+"""
